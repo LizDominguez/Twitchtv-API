f34d60cdc57a1efe64362eb93ca56d08eb042a27
Liz Dominguez <liz.dominguez.leon@gmail.com>
2017-05-30 11:47:50 -0400

    added API call and styled it

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
+  var users = ["FreeCodeCamp", "Meluist", "EnJoy_The_Silence_", "Freack1982"]; 
+  
+$.ajax({
+ type: 'GET',
+ url: 'https://api.twitch.tv/kraken/streams/' + users,
+ headers: {
+   'Client-ID': 'magndgamnhcvyyvfed0xuscpsswl7a'
+ },
+ success: function(data) {
+   console.log(data);
+ }
+});
+
+
+$(document).ready(function () {  
+  "use strict";
+  
+});
